efef204514362548d30924a9c176a7c74191bd58
geekydiva <jackie.mccoy@ollion.com>
2026-01-26 09:08:01 -0500

    changing main to have variables

--- examples/vpc-only/main.tf
+++ examples/vpc-only/main.tf
@@ -1,8 +1,8 @@
 module "vpc" {
   source              = "../../../modules/vpc"
   vpc_cidr            = var.vpc_cidr
-  public_subnet_cidrs = ["10.23.0.0/23","10.23.2.0/23","10.23.4.0/23"]
-  private_subnet_cidrs= ["10.23.16.0/20", "10.23.32.0/20", "10.23.48.0/20"]
+  public_subnet_cidrs = var.public_subnet_cidrs
+  private_subnet_cidrs= var.private_subnet_cidrs
   name_prefix         = var.name_prefix
   tags                = var.tags
 }
